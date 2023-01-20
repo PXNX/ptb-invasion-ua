e9c1986da06fa2081206c88773793dac8db9a37f
pentexnyx <loool1912@web.de>
2023-01-20 01:21:23 +0100

    fix

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 import logging
+import os
+from datetime import datetime
 
 from telegram.constants import ParseMode
-from telegram.ext import ApplicationBuilder, filters, Defaults
+from telegram.ext import ApplicationBuilder, filters, Defaults, CommandHandler, PicklePersistence
 from telegram.ext import MessageHandler
 
 from config import CHANNEL, TOKEN, TEST_MODE, PORT
-from messages import append_footer, append_footer_text
+from messages import append_footer, append_footer_text, append_footer_forward
 
-logging.basicConfig(format="%(asctime)s -  %(levelname)s - [%(filename)s:%(lineno)s - %(funcName)20s()]: %(message)s ",
-                    level=logging.INFO)
+LOG_FILENAME = r'C:\Users\Pentex\PycharmProjects\ptb-invasion-ua\logs\log-' + f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.out"
+if not os.path.exists(LOG_FILENAME):
+    open(LOG_FILENAME, "w")
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - [%(filename)s:%(lineno)s - %(funcName)20s()]: %(message)s ",
+    level=logging.INFO, filename=LOG_FILENAME
+)
 
 
 def main():
     app = ApplicationBuilder() \
         .token(TOKEN) \
         .defaults(Defaults(parse_mode=ParseMode.HTML, disable_web_page_preview=True)) \
+        .persistence(PicklePersistence(filepath="persistence")) \
         .build()
 
     # This filters for Photos, Videos and Gifs in your Channel. Normal Text-messages are ignored.
     app.add_handler(MessageHandler(
-        (filters.PHOTO | filters.VIDEO | filters.ANIMATION) & filters.Chat(chat_id=CHANNEL) & ~filters.FORWARDED,
-        append_footer))
+        filters.UpdateType.CHANNEL_POST & (filters.PHOTO | filters.VIDEO | filters.ANIMATION) & filters.Chat(
+            chat_id=CHANNEL) & ~filters.FORWARDED, append_footer))
 
-    app.add_handler(
-        MessageHandler(filters.TEXT & filters.Chat(chat_id=CHANNEL) & ~filters.FORWARDED, append_footer_text))
+    app.add_handler(MessageHandler(
+        filters.UpdateType.CHANNEL_POST & filters.TEXT & filters.Chat(chat_id=CHANNEL) & ~filters.FORWARDED,
+        append_footer_text))
+
+    app.add_handler(MessageHandler(
+        filters.UpdateType.CHANNEL_POST &  (filters.PHOTO | filters.VIDEO | filters.ANIMATION) & filters.Chat(chat_id=CHANNEL) & filters.FORWARDED,
+        append_footer_forward))
 
     if TEST_MODE:
         print("### Start Local ###")
--- messages.py
+++ messages.py
@@ -1,7 +1,10 @@
+import datetime
 import logging
+import re
+import time as t
 
 from telegram import Update
-from telegram.ext import CallbackContext, ContextTypes, JobQueue
+from telegram.ext import CallbackContext, ContextTypes
 
 from config import CHANNEL, FOOTER
 
@@ -9,17 +12,37 @@ from config import CHANNEL, FOOTER
 async def append_footer_text(update: Update, _: ContextTypes.DEFAULT_TYPE):
     print(f"append footer text :: {update}")
 
-    await update.channel_post.edit_text(update.channel_post.text_html_urled + FOOTER)
+    text = update.channel_post.text_html_urled
+
+    await update.channel_post.edit_text(text + FOOTER)
+
+
+async def append_footer_forward(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"append footer forward :: {update}")
+
+    time = t.time()
+    is_valid = update.channel_post.forward_from_chat.id == -1001839268196 and update.channel_post.media_group_id is None
+
+    print(is_valid, time)
+    print(datetime.datetime.now().timestamp(), context.bot_data.get("latest", time))
+
+    if is_valid and time < context.bot_data.get("latest", time):
+        text = re.sub(r"\s*$", "", re.sub(r"Quelle:(.|\s)*", "", update.channel_post.caption_html_urled)) + FOOTER
+        await update.channel_post.copy(CHANNEL, text)
+       # await update.channel_post.delete()
+        print("-- delete -")
+
+    else:
+        context.bot_data["latest"] = time + 5400
 
 
 async def append_footer(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f"append footer :: {update}")
 
-    mg = update.channel_post.media_group_id
     original_caption = update.channel_post.caption_html_urled
 
     # it's a single message, not an album - hence does not contain a mediagroup_id
-    if mg is None:
+    if update.channel_post.media_group_id is None:
 
         if original_caption is None:
             original_caption = ""
@@ -34,15 +57,15 @@ async def append_footer(update: Update, context: ContextTypes.DEFAULT_TYPE):
         prev_list = {update.channel_post.id}
 
         # go through already present jobs and get their dat
-        if context.job_queue is not None:
-            for job in context.job_queue.get_jobs_by_name(mg):
-                logging.info(f"Removed job {job}")
 
-                for post_id in job.data["ids"]:
-                    prev_list.add(post_id)
+        for job in context.job_queue.get_jobs_by_name(update.channel_post.media_group_id):
+            logging.info(f"Removed job {job}")
 
-                if job.data["text"] is not None:
-                    original_caption = job.data["text"]
+            for post_id in job.data["ids"]:
+                prev_list.add(post_id)
+
+            if "text" in job.data:
+                original_caption = job.data["text"]
 
                 job.schedule_removal()
 
@@ -52,7 +75,7 @@ async def append_footer(update: Update, context: ContextTypes.DEFAULT_TYPE):
             data["text"] = original_caption
 
         # 10 seconds is maybe too high, but better waiting than achieving incorrect results because it may take longer
-        context.job_queue.run_once(append_footer_mg, 10, data, mg)
+        context.job_queue.run_once(append_footer_mg, 10, data, update.channel_post.media_group_id)
 
 
 async def append_footer_mg(context: CallbackContext):
